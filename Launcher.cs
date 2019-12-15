6769c054175a427371fcc8c10ab3607c8f2a8875
Yaroslav Skrebets <iam@sonicxd2.ru>
2019-12-16 02:07:30 +0300

    Add Launcher

---
+++ Launcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOP
+{
+    public class Launcher
+    {
+        static void Main(string[] args)
+        {
+            INIConfig config = new INIConfig("test.ini");
+            var s = config.get("_", "1873021582");
+            Console.Out.WriteLine(s);
+            
+            INIConfig d = new INIConfig("ad.ini");
+            Console.WriteLine(d.get("C", "C"));
+        }
+    }
+}
